3b3058f4de3c38857adb8f27d6424071728a9cca
juanmaynard91 <juanmaynard91@gmail.com>
2023-02-24 00:42:27 -0300

    intentando hacer funcionar el mic en el celular

--- src/components/buscarPokemon.vue
+++ src/components/buscarPokemon.vue
@@ -13,6 +13,12 @@
             <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
           </svg>
         </button>
+        <button class="btn btn-secondary btn_mic border_black" type="button" id="button-addon2" @click="stopRecognition">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16">
+            <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3z"/>
+            <path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z"/>
+          </svg>
+        </button>
       </div>
     </form>
 
@@ -87,6 +93,10 @@ recognition.onresult = (event) => {
 
 const startRecognition = () => {
   recognition.start();
+};
+
+const stopRecognition = () => {
+  recognition.stop();
 };
 
 const cargarPokemonesMicrofono = async () => {

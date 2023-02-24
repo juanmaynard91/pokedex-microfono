e26e4ff477e37cf3e37b0c5d1cfa3ccc2197fd69
juanmaynard91 <juanmaynard91@gmail.com>
2023-02-24 00:57:27 -0300

    anda microfono por favor te lo pido

--- src/components/buscarPokemon.vue
+++ src/components/buscarPokemon.vue
@@ -6,17 +6,11 @@
   <main class="container">
     <form @submit.prevent="buscarPokemon">
       <div class="input-group mt-3 mb-3">
-        <input type="text" class="form-control input border_black" placeholder="ingrese el nombre del pokemon.." aria-label="Recipient's username" aria-describedby="button-addon2" v-model.trim="buscar" />
-        <button class="btn btn-secondary btn_mic border_black" type="button" id="button-addon2" @click="startRecognition">
+        <input type="text" class="form-control input border_black" placeholder="ingrese el nombre del pokemon.." aria-label="Recipient's username" aria-describedby="button-addon2" v-model.trim="buscarConInput" />
+        <button class="btn btn-secondary btn_mic border_black" type="button" id="button-addon2" @click="activarMicrofono">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16">
             <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z" />
             <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
-          </svg>
-        </button>
-        <button class="btn btn-secondary btn_mic border_black" type="button" id="button-addon2" @click="stopRecognition">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16">
-            <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3z"/>
-            <path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z"/>
           </svg>
         </button>
       </div>
@@ -42,9 +36,9 @@ import Swal from "sweetalert2"; // alert
 
 const pokemones = ref({});
 const pokeApi = "https://pokeapi.co/api/v2/pokemon";
-const buscar = ref("");
+const buscarConInput = ref("");
 const stats = ref([]);
-const transcript = ref("");
+const buscarConMicrofono = ref("");
 const recognition = new window.webkitSpeechRecognition();
 
 //la solucion para que la pagina tarde en cargar
@@ -53,7 +47,7 @@ await new Promise((res) => setTimeout(res, 2000));
 const cargarPokemonesInput = async () => {
   try {
     //muestra la imagen hasta el 897
-    const res = await fetch(`${pokeApi}/${buscar.value}`);
+    const res = await fetch(`${pokeApi}/${buscarConInput.value}`);
     const data = await res.json();
     pokemones.value = data;
 
@@ -72,11 +66,11 @@ const cargarPokemonesInput = async () => {
 };
 
 const buscarPokemon = () => {
-  if (buscar.value !== "") {
+  if (buscarConInput.value !== "") {
     //por si el usuario ingresa la primera letra en mayuscula haga la busqueda igual
-    buscar.value = buscar.value.toLocaleLowerCase();
+    buscarConInput.value = buscarConInput.value.toLocaleLowerCase();
     cargarPokemonesInput();
-    buscar.value = "";
+    buscarConInput.value = "";
   } else {
     pokemones.value = [];
   }
@@ -87,21 +81,21 @@ recognition.interimResults = false;
 recognition.lang = "es-ES";
 
 recognition.onresult = (event) => {
-  transcript.value = event.results[0][0].transcript.toLowerCase();
+  buscarConMicrofono.value = event.results[0][0].transcript.toLowerCase();
+  console.log(buscarConMicrofono.value)
   cargarPokemonesMicrofono();
 };
 
-const startRecognition = () => {
+const activarMicrofono = () => {
   recognition.start();
-};
-
-const stopRecognition = () => {
+  setTimeout(()=>{
   recognition.stop();
+  },2000)
 };
 
 const cargarPokemonesMicrofono = async () => {
   try {
-    const res = await fetch(`${pokeApi}/${transcript.value}`);
+    const res = await fetch(`${pokeApi}/${buscarConMicrofono.value}`);
     const data = await res.json();
     pokemones.value = data;
 

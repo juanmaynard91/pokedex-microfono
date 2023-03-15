cd11790f9424499a296ad09de12c09dd68d24287
juanmaynard91 <juanmaynard91@gmail.com>
2023-03-15 19:08:22 -0300

    configurando vite.config.js para comprimir la app

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import compression from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression'; //npm install vite-plugin-compression --save-dev para comprimir los archivos
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
   compression({
     ext: '.br, .js, .css, .html, .svg', // Utiliza el algoritmo de compresión Brotli
+    filter: (req, res) => {
+      if (req.headers['accept-encoding']) {
+        return /text|application|image/i.test(req.headers['accept-encoding']);
+      }
+      return false;
+    },
+    threshold: 10240,
+    gzip: {
+      flush: require('zlib').constants.Z_SYNC_FLUSH
+    },
+    deflate: {
+      flush: require('zlib').constants.Z_SYNC_FLUSH,
+    },
+    brotli: {
+      quality: 11,
+    },
   }),],
   base: "/pokedex-microfono/"
 })

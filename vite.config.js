9d0a925124499fbe16eee3e3fc85a099b4ee0db7
juanmaynard91 <juanmaynard91@gmail.com>
2023-03-17 16:56:30 -0300

    agregando optimizadores de css y js de chat-gpt

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import compression from 'vite-plugin-compression'; //npm install vite-plugin-compression --save-dev para comprimir los archivos
+import compression from 'vite-plugin-compression'; //npm install vite-plugin-compression --save-dev (para comprimir los archivos)
+import cssnano from 'cssnano'; //npm install cssnano --save-dev (comprime css)
+import { terser } from 'rollup-plugin-terser'; //npm install terser --save-dev - (SOLUCION: npm install rollup-plugin-terser --save-dev) - (comprime js)
 
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [vue(), terser(), cssnano(),
   compression({
     ext: '.br, .js, .css, .html, .svg', // Utiliza el algoritmo de compresión Brotli
     threshold: 10240,
